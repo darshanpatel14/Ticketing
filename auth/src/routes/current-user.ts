4cc8ca9da23d968f2d59536ce81c6ee1c401e3b8
Darshan Bhut <darshanbhut14@gmail.com>
2022-07-09 12:15:31 +0530

    added new errors and middleware for current-user

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,8 +1,16 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import { currentUser } from "../middlewares/current-user";
+import { requireAuth } from "../middlewares/require-auth";
 const router = express.Router();
 
-router.get('/api/users/currentuser', (req, res) => {
-  res.send('Hi there');
-})
+router.get(
+  "/api/users/currentUser",
+  currentUser,
+  requireAuth,
+  (req: Request, res: Response) => {
+    res.send({ currentUser: req.currentUser || null });
+  }
+);
 
 export { router as currentUserRouter };
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -5,7 +5,7 @@ import { User } from "../models/user";
 import { RequestValidationError } from "../errors/request-validation-error";
 import { DatabaseConnectionError } from "../errors/database-connection-error";
 import { BadRequestError } from "../errors/bad-request-error";
-
+import { validateRequest } from '../middlewares/validate-request';
 const router = express.Router();
 
 router.post(
@@ -16,13 +16,8 @@ router.post(
       .trim()
       .isLength({ min: 4, max: 20 })
       .withMessage("Password Must Be Between 4 and 20 Characters"),
-  ],
+  ], validateRequest,
   async (req: Request, res: Response) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      throw new RequestValidationError(errors.array());
-    }
     const { email, password } = req.body;
 
     const existingUser = await User.findOne({ email });
